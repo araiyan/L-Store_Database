4739c0bfe226e5e495e7fe2df6a854a9215e1773
Raiyan1000 <bandhan.sazid@gmail.com>
2025-01-26 20:31:05 -0800

    Initial Table (Unfinished)
    Needs Page implementation. Page Range could also be helpful but needs major refactoring. Need clarity on Merging table before continuation.

--- lstore/page.py
+++ lstore/page.py
@@ -13,3 +13,15 @@ class Page:
         self.num_records += 1
         pass
 
+    def write_precise(self, index, value):
+        '''
+        This function should be able to write data on a precise index inside the table.
+        Useful for changing the indirection column on the base page
+        '''
+        pass
+
+    def get(self, index):
+        '''This funciton should be able to grab a data located at a certain index in the page'''
+        pass
+
+    
