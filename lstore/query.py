deb71b8b82ec9e01d69318ac2f7715c35bfc1d32
Lyko <83723448+Lyko2061@users.noreply.github.com>
2025-02-06 18:09:52 -0800

    Generalized `__getLatestTailRid()` to `__getLatestConRid()`
    Since we can get the latest tail_rid from the base page or the latest consolidated page, I generalized it so the helper function returns the consolidated page instead. 
    
    Generalization allows me to update the indirection column of the latest consolidated/base record to point to the updated tail while maintaining the option of getting the latest tail from the newest record. It can be applied to other functions in `query.py` as well.

--- lstore/query.py
+++ lstore/query.py
@@ -1,5 +1,6 @@
 from lstore.table import Table, Record
 from lstore.index import Index
+from lstore.page import Page
 from lstore.config import *
 
 
@@ -46,53 +47,7 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select(self, search_key, search_key_index, projected_columns_index):
-        try:
-            # retrieve a list of RIDs that contain the "search_key" value within the column as defined by "search_key_index"
-            rid_list = self.table.index.locate(search_key_index, search_key)
-
-            # if there exists no RIDs that match the given parameters, return False
-            if not rid_list:
-                return False
-
-            record_objs = []
-
-            # iterate through all desired RIDs
-            for rid in rid_list:
-                base_page_location = self.table.page_directory.get(rid, None)
-                if base_page_location is None:
-                    continue
-                    
-                # store the (base) page# and index# that the RID/row is located
-                base_page_number, base_page_index = base_page_location[0]
-
-                # store base values within projected columns - we will update values with latest data as necessary 
-                record_columns = [
-                    self.table.base_pages[NUM_HIDDEN_COLUMNS + i][base_page_number].get(base_page_index)
-                    for i in range(len(projected_columns_index)) if projected_columns_index[i] == 1
-                ]
-
-                # retrieve indirection RID so that we can traverse through updated pages/records
-                indirection_rid = self.table.base_pages[INDIRECTION_COLUMN][base_page_number].get(base_page_index)
-
-                while indirection_rid != rid:
-                    # grab schema encoding value to determine whether or not a given column within a tail record was updated
-                    schema_encoding = self.table.__grab_tail_value_from_rid(indirection_rid, SCHEMA_ENCODING_COLUMN)
-                    
-                    for i in range(len(projected_columns_index)):
-                        if projected_columns_index[i] == 1 and ((schema_encoding >> i) & 1) == 1:
-                            record_columns[i] = self.table.__grab_tail_value_from_rid(indirection_rid, NUM_HIDDEN_COLUMNS + i) 
-                            projected_columns_index[i] = 0 # we no longer want to update this column - otherwise, we would be overriding newer data with old data
-
-                    # update indirection_rid to the next (previous version) within chain of tail records
-                    indirection_rid = self.table.__grab_tail_value_from_rid(indirection_rid, INDIRECTION_COLUMN)
-
-                record_objs.append(Record(rid, search_key, record_columns))
-
-            return record_objs if record_objs else False
-        
-        except Exception as e:
-            print(f"Error in select: {e}")
-            return False
+        pass
 
     
     """
@@ -106,61 +61,61 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select_version(self, search_key, search_key_index, projected_columns_index, relative_version):
-        try:
-            rid_list = self.table.index.locate(search_key_index, search_key)
-
-            if not rid_list:
-                return False
-            
-            record_objs = []
-
-            for rid in rid_list:
-                base_page_location = self.table.page_directory.get(rid, None)
-                if base_page_location is None:
-                    continue
-
-                base_page_number, base_page_index = base_page_location[0]
-
-                indirection_rid = self.table.base_pages[INDIRECTION_COLUMN][base_page_number].get(base_page_index)
-
-                # traverse through the tail pages based on the relative version
-                current_version = 0
-                while current_version > relative_version and indirection_rid != rid:
-                    current_version -= 1
-                    indirection_rid = self.table.__grab_tail_value_from_rid(indirection_rid, INDIRECTION_COLUMN)
-                
-                # unsuccessful in finding an older version - this implies that indirection_rid == rid and we can
-                # thus directly retrieve from the base page
-                if current_version > relative_version:
-                    record_columns = [
-                    self.table.base_pages[NUM_HIDDEN_COLUMNS + i][base_page_number].get(base_page_index)
-                    for i in range(len(projected_columns_index)) if projected_columns_index[i] == 1
-                ]
-                # otherwise, retrieve from the relative version
-                else:
-                    record_columns = [
-                    self.table.__grab_tail_value_from_rid(indirection_rid, NUM_HIDDEN_COLUMNS + i)
-                    if projected_columns_index[i] == 1 else None
-                    for i in range(len(projected_columns_index))
-                ]
-                    
-                record_objs.append(Record(rid, search_key, record_columns))
-                
-            return record_objs if record_objs else False
-        
-        except Exception as e:
-            print(f"Error during select_version: {e}")
-            return False
-
-    
-    """
-    # Update a record with specified key and columns
-    # Returns True if update is succesful
-    # Returns False if no records exist with given key or if the target record cannot be accessed due to 2PL locking
-    """
-    def update(self, primary_key, *columns):
         pass
 
+    
+        """
+    # Update a record with specified key and columns
+    # Returns True if update is succesful
+    # Returns False if no records exist with given key or if the target record cannot be accessed due to 2PL locking (Ignore this for now)
+    """
+    def update(self, primary_key, *columns):
+
+        # We want to see if the record exists with the specified key; no point in continuing further without checking
+        rid_location = self.table.index.locate(self.table.key, primary_key)
+        if(rid_location is None):
+            return False
+        
+        # The tail should have the same number of columns as the other pages so we should be multiplying by the total_num_columns value
+        # Multiplying by [None] atm since the columns don't have an assigned size
+        new_columns = [None] * self.table.total_num_columns
+
+        # Insert values into the new column
+        for i, value in enumerate(columns):
+
+            # If we're modifying the primary_key then this update should be stopped since we can't change the primary_key column
+            if(i == self.table.key or value == primary_key):
+                return False
+            
+            new_columns[NUM_HIDDEN_COLUMNS + i] = value
+
+        # schema = 0; schema += 2^i or bit shift schema += 1 << i
+
+        # We want to set indirection for temp tail record to be the previous tail_rid --> go to base/cons to get latest
+        cons_rid = self.__getLatestConRid(rid_location)
+        cons_page_index, cons_page_slot = self.table.page_directory[cons_rid][0]
+        prev_tail_rid = self.table.base_pages[INDIRECTION_COLUMN][cons_page_index].get(cons_page_slot)
+        new_columns[INDIRECTION_COLUMN] = prev_tail_rid
+
+
+        # Create new record and initialize it into the pd
+        new_record = Record(rid = -1, key = primary_key, columns = new_columns)
+
+        self.table.assign_rid_to_record(new_record)
+        self.table.page_directory[new_record.rid] = [None] * self.table.total_num_columns
+        new_columns[RID_COLUMN] =  new_record.rid
+        self.__writeTailRecord(new_record)
+
+
+        # Updates the indirection column on the base/cons page for new tail page
+        page_index, page_slot = self.table.page_directory[cons_rid][INDIRECTION_COLUMN]
+        self.table.base_pages[INDIRECTION_COLUMN][page_index].write_precise(page_slot, new_record.rid)        
+
+        # Update indices
+        self.table.index.update_all_indices(primary_key, *new_columns)
+
+        # Update successful
+        return True
     
     """
     :param start_range: int         # Start of the key range to aggregate 
@@ -188,7 +143,7 @@ class Query:
 
     
     """
-    incremenets one column of the record
+    Increments one column of the record
     this implementation should work if your select and update queries already work
     :param key: the primary of key of the record to increment
     :param column: the column to increment
@@ -203,3 +158,57 @@ class Query:
             u = self.update(key, *updated_columns)
             return u
         return False
+    
+
+    """
+    Starting from the base record’s indirection pointer, we want to go through all the 
+    base/consolidated records. Iterate through with until the column_page_locations tuple
+    is greater than 1, signifying the end of the base/consolidated records
+    """
+    def __getLatestConRid(self, base_rid):
+
+        # We are given the base rid so we want to start from here and iterate through all the consolidated pages if there are any
+        base_page_index, base_page_slot = self.table.page_directory[base_rid][0]
+
+        # Assuming that there are none, we want to set the indirection rid to be the base page's; this would point to the latest tail
+        indirection_rid = self.table.base_pages[INDIRECTION_COLUMN][base_page_index].get(base_page_slot)
+
+        # If no cons; newest is base
+        previous_rid = base_rid
+
+        ## This is moreso just for checking tuple length to ensure we're looking at a base/consolidated page
+        column_page_locations = self.table.page_directory[indirection_rid]
+
+        # If there are consolidated pages w/ we walk them for indirection ptr that points to the latest tail
+        while(indirection_rid != base_rid and len(column_page_locations) == 1):
+            
+            previous_rid = indirection_rid
+            page_index, page_slot = column_page_locations[0]
+            indirection_rid = self.table.base_pages[INDIRECTION_COLUMN][page_index].get(page_slot)
+            column_page_locations = self.table.page_directory[indirection_rid]
+        
+        return previous_rid
+    
+
+    """Helper function that writes the new record into the appropriate tail pages."""
+    def __writeTailRecord(self, new_record):
+
+        for i in range(self.table.total_num_columns):
+            
+            # Skip columns we're not updating
+            if(new_record.columns[i] is None):
+                continue
+
+            # If current tail page is full, append a new page
+            if(not self.table.tail_pages[i][-1].has_capacity()):
+                self.table.tail_pages[i].append(Page())
+
+            # Current page is the latest tail page
+            curr_page: Page = self.table.tail_pages[i][-1]
+            
+            # Writing to current page, we get the position of the tail record in the page & index of the page
+            pos = curr_page.write(new_record.columns[i])
+            tail_page_index = len(self.table.tail_pages[i]) - 1
+
+            # Write this to the pd
+            self.table.page_directory[new_record.rid][i] = (tail_page_index, pos)
