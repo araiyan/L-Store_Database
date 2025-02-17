17762462d965e45ed772d3e979811014b5f94596
Leo Ying <leoleoying@gmail.com>
2025-02-16 23:59:49 -0800

    Inital open impl

--- lstore/db.py
+++ lstore/db.py
@@ -1,14 +1,70 @@
+import os
+import json
 from lstore.table import Table
+from lstore.bufferpool import Bufferpool
+from lstore.index import Index
 
 class Database():
 
     def __init__(self):
         self.tables:dict = {}
+        self.path = None
+        self.bufferpool = None
         pass
 
-    # Not required for milestone1
     def open(self, path):
-        pass
+        """Loads database from disk and restores tables, indexes, and pages"""
+        self.path = path
+        self.bufferpool = Bufferpool(path)
+
+        # create new path if database path doesn't exist
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        # grab table metadata - we assume that input path contains subpath to tables.json
+        tables_metadata_path = os.path.join(path, "tables.json")
+        if os.path.exists(tables_metadata_path):
+            with open(tables_metadata_path, "r") as file:
+                tables_metadata = json.load(file)
+
+                # loops through tables and adds them to the self.tables dictionary
+                for table_name, table_info in tables_metadata.items():
+                    table = Table(table_name, table_info["num_columns"], table_info["key_index"])
+                    self.tables[table_name] = table
+
+                    # restores page directory for table
+                    table.page_directory = table_info["page_directory"]
+
+                    # initialize indexes for table
+                    table.index = Index(table)
+                    for column_idx, indexed_values in table_info["indexes"].items():
+                        table.index.create_index(int(column_idx))
+
+                        # loop through inner dictionary and load stored values into the created index
+                        for value, rids in indexed_values.items():
+                            for rid in rids:
+                                table.index.insert_to_index(int(column_idx, value, rid))
+
+                    # load pages from disk
+                    table_path = os.path.join(path, table_name)
+                    if os.path.exists(table_path):
+                        for page_range in os.listdir(table_path):
+                            page_range_path = os.path.join(table_path, page_range)
+                            if os.path.isdir(page_range_path) and page_range.startswith("PageRange_"):
+
+                                # grab pange range index from path name
+                                page_range_index = int(page_range.split("_")[1])
+
+                                for file in os.listdir(page_range_path):
+                                    if file.startswith("Page_") and file.endswith(".bin"):
+                                        parts = file.split("_")
+                                        column_index = int(parts[1])
+                                        page_index = int(parts[2].split(".")[0])
+
+                                        # load page into bufferpool
+                                        page, frame_num = self.bufferpool.get(page_range_index, column_index, page_index)
+                                        if page is not None:
+                                            print(f"Loaded {file} into bufferpool for table {table_name}")
 
     def close(self):
         pass
