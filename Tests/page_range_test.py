33944f637760deadae1a554ac44c1dd977673f2f
Raiyan1000 <bandhan.sazid@gmail.com>
2025-02-21 21:25:57 -0800

    page range tested

--- Tests/page_range_test.py
+++ Tests/page_range_test.py
@@ -14,16 +14,29 @@ class TestPageRange(unittest.TestCase):
   
     def __init__(self, methodName = "runTest"):
         super().__init__(methodName)
-        self.num_records = 24
-        self.num_columns = 3
-        self.table = Table("PageRangeTestTable", self.num_columns, 0, "TestDB")
+        self.num_records = 2400
+        self.num_columns = 5
+        self.table:Table = Table("PageRangeTestTable", self.num_columns, 0, "TestDB")
         self.bufferpool = BufferPool(self.table.table_path)
         self.page_range = PageRange(0, self.num_columns, self.bufferpool)
         self.records = {}
 
         for i in range(self.num_records):
             self.records[i] = [randint(0, 100) for _ in range(self.num_columns)]
-            print(f"Created Record: {self.records[i]}")
+
+        self.updated_records = {}
+        self.update_tail_records = {}
+        for key in self.records:
+            updated_columns = [None, None, None, None, None]
+            self.updated_records[key] = self.records[key].copy()
+            for i in range(randint(0, 3), randint(3, self.table.num_columns)):
+                # updated value
+                value = randint(0, 20)
+                updated_columns[i] = value
+                # update our test directory
+                self.updated_records[key][i] = value
+            
+            self.update_tail_records[key] = updated_columns
 
         print("\n")
 
@@ -46,12 +59,18 @@ class TestPageRange(unittest.TestCase):
         for i in range(self.num_records):
             #print(f"Writing Record to PageRange: {self.records[i]}")
             logical_rid = self.page_range.assign_logical_rid()
-            self.page_range.write_tail_record(logical_rid, *self.records[i])
+            hidden_columns = [0] * NUM_HIDDEN_COLUMNS
+            all_columns = hidden_columns + self.update_tail_records[i]
+            self.page_range.write_tail_record(logical_rid, *all_columns)
 
         # ensure tail page indexes are updated
         self.assertNotEqual(self.page_range.tail_page_index, [0] * self.num_columns)
         # logical directory should be updated on tail record write
         self.assertNotEqual(self.page_range.logical_directory, {})
+
+        for i in range(self.table.num_columns):
+            if (self.page_range.logical_directory[self.page_range.logical_rid_index - 1][i] != None):
+                self.assertLess(self.page_range.logical_directory[self.page_range.logical_rid_index - 1][i], self.page_range.logical_rid_index)
 
         print("Test Page Range Tail Record Write Passed")
 
