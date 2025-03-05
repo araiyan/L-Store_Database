e0ea8d481be148795256d3c8389de8cf362a3a96
Leo Ying <leoleoying@gmail.com>
2025-03-04 18:47:26 -0800

    Initial log impl

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -8,7 +8,7 @@ class Transaction:
     """
     def __init__(self):
         self.queries = []
-        pass
+        self.log = []
 
     """
     # Adds the given query to this transaction
@@ -18,17 +18,21 @@ class Transaction:
     # t.add_query(q.update, grades_table, 0, *[None, 1, None, 2, None])
     """
     def add_query(self, query, table, *args):
-        self.queries.append((query, args))
+        self.queries.append((query, table, args))
         # use grades_table for aborting
 
         
     # If you choose to implement this differently this method must still return True if transaction commits or False on abort
     def run(self):
-        for query, args in self.queries:
+        for query, table, args in self.queries:
             result = query(*args)
             # If the query has failed the transaction should abort
             if result == False:
                 return self.abort()
+            
+            # log successful operations for future potential rollback
+            self.log.append((query, table, args))
+
         return self.commit()
 
     
@@ -38,6 +42,8 @@ class Transaction:
 
     
     def commit(self):
-        # TODO: commit to database
+
+        # clear log list after commiting changes
+        self.log.clear()
         return True
 
